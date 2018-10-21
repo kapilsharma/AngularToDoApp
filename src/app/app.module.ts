da4050679f08898da9c64d7f46ae22a1d2f7d4bc
Kapil Sharma <kapil@kapilsharma.info>
2018-10-21 11:27:18 +0530

    custom property

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { ToDoComponent } from './to-do/to-do.component';
+import { ListComponent } from './to-do/list/list.component';
+import { CreateComponent } from './to-do/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ToDoComponent
+    ToDoComponent,
+    ListComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule
